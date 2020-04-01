9d6f089093824830ad89c45d0a748200b2389ce8
dvfcfbgv <21ianh1@tas.tw>
2020-04-02 01:12:34 +0800

    feat: character movements

---
+++ Assets/Scripts/Controls/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    public Transform target, player;
+    float mouseX, mouseY;
+
+    void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
 
