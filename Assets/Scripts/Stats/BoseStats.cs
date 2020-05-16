52a14518d9159e40a78639ae591469e1f1830071
Ian Huang <21ianh1@students.tas.tw>
2020-05-17 01:20:52 +0800

    feat: icons

---
+++ Assets/Scripts/Stats/BoseStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoseStats : EnemyStats
+{
+    public GameObject villain;
+
+    public override void Die()
+    {
+        base.Die();
+
+        villain.SetActive(true);
+
+        // DO THE TRANSITIONS HERE;
+    }
+}
